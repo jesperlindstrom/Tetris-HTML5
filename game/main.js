8a82796eae551bfbc1afc32c53b712ed2aecd66a
Jesper Lindström <raxezdev@gmail.com>
2014-08-23 16:04:55 +0200

    Restructure

--- game/main.js
+++ game/main.js
@@ -1,32 +1,20 @@
-window.onload = function() {
-	var game = new Phaser.Game(700, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-	var logo;
-	var platforms;
+var game = new Phaser.Game(800, 480, Phaser.AUTO, 'game');
 
-	function preload () {
-		game.load.image('logo', 'phaser.png');
-		game.load.image('ground', 'ground.png');
-	}
+var main = {
+	preload: function() {
+		game.time.advancedTiming = true;
+	},
 
-	function create() {
-		game.physics.startSystem(Phaser.Physics.ARCADE);
+	create: function() {
+		
+	},
 
-		logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-		logo.anchor.setTo(0.5, 0.5);
-		game.physics.arcade.enable(logo);
-		logo.body.gravity.y = 900;
-		logo.body.bounce.y = 1;
-		logo.body.collideWorldBounds = true;
+	update: function() { },
 
-		platforms = game.add.group();
-		platforms.enableBody = true;
-
-		var ground = platforms.create(0, game.world.height - 64, 'ground');
-		ground.scale.setTo(11, 2);
-		ground.body.immovable = true;
-	}
-
-	function update() {
-		game.physics.arcade.collide(logo, platforms);
+	render: function() {
+		game.debug.text(game.time.fps || '--', 2, 14, "#00ff00");
 	}
 };
+
+game.state.add('main', main);
+game.state.start('main');
