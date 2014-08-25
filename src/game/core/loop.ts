6c21f24be4f1e4bf9f777682ac9136a738dae5ff
Jesper Lindström <raxezdev@gmail.com>
2014-08-25 12:15:53 +0200

    Fixed loop

--- src/game/core/loop.ts
+++ src/game/core/loop.ts
@@ -6,20 +6,33 @@ module Core {
 	export class Loop {
 		public isRunning: boolean = true;
 		private updateCalls: Array<UpdateCall> = [];
+		private lastFrame: number = 0.0;
+		private FPS: number = 0;
 
+		/**
+		 * Start the loop
+		 */
 		public start() {
 			this.isRunning = true;
 			this.tick(0);
 		}
 
+		/**
+		 * Pause the loop
+		 */
 		public stop() {
 			this.isRunning = false;
 		}
 
+		/**
+		 * Run the next tick of the loop
+		 * @param Number time
+		 */
 		private tick(time: number) {
-			console.log(time);
-			console.log(this);
-			// todo: wrong scope!
+			var delta: number = time - this.lastFrame; // 60 FPS ≈ 16.6667
+
+			this.FPS = Math.round(1 / delta * 1000);
+			this.lastFrame = time;
 
 			if (this.updateCalls.length) {
 				this.updateCalls.forEach(method => {
@@ -28,7 +41,9 @@ module Core {
 			}
 
 			if (this.isRunning) {
-				window.requestAnimationFrame(this.tick);
+				window.requestAnimationFrame((time) => {
+					this.tick.apply(this, [time]);
+				});
 			}
 		}
 
