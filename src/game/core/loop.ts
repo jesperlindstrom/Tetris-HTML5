eac19c7633bf2ba9a01df5b518a3788841063f93
Jesper Lindström <raxezdev@gmail.com>
2014-08-25 14:31:09 +0200

    Improved loop, added 'rate' value

--- src/game/core/loop.ts
+++ src/game/core/loop.ts
@@ -5,9 +5,24 @@ module Core {
 
 	export class Loop {
 		public isRunning: boolean = true;
+		public FPS: number = 0;
+		public rate: number = 0.0;
 		private updateCalls: Array<UpdateCall> = [];
 		private lastFrame: number = 0.0;
-		private FPS: number = 0;
+
+		constructor() {
+			// Pause when the window is unfocused
+			window.onblur = (e) => {
+				this.stop();
+			};
+
+			// Start when the window is focused again
+			window.onfocus = (e) => {
+				if (!this.isRunning) {
+					this.start();
+				}
+			};
+		}
 
 		/**
 		 * Start the loop
@@ -22,6 +37,8 @@ module Core {
 		 */
 		public stop() {
 			this.isRunning = false;
+			this.rate = 0;
+			this.lastFrame = 0;
 		}
 
 		/**
@@ -29,19 +46,31 @@ module Core {
 		 * @param Number time
 		 */
 		private tick(time: number) {
-			var delta: number = time - this.lastFrame; // 60 FPS ≈ 16.6667
+			if (this.lastFrame && time) {
+				var delta: number = time - this.lastFrame; // 60 FPS ≈ 16.6667
 
-			this.FPS = Math.round(1 / delta * 1000);
+				this.rate = Math.round(delta / 16.67 * 100) / 100;
+				this.FPS = Math.round(1 / delta * 1000);
+			} else {
+				Core.Log.info('Skipped a frame', 'Core/Loop');
+				this.rate = 0;
+				this.FPS = 0;
+			}
+
 			this.lastFrame = time;
 
+			// Run all update calls
 			if (this.updateCalls.length) {
 				this.updateCalls.forEach(method => {
-					method(time);
+					if (typeof method == 'function') {
+						method(this.rate);
+					}
 				});
 			}
 
+			// Request the next frame
 			if (this.isRunning) {
-				window.requestAnimationFrame((time) => {
+				window.requestAnimationFrame(time => {
 					this.tick.apply(this, [time]);
 				});
 			}
@@ -50,9 +79,25 @@ module Core {
 		/**
 		 * Add game logic to be run on each frame
 		 * @param Function method(number)
+		 * @return Number
 		 */
-		public onUpdate(method: (number) => void) {
-			this.updateCalls.push(method);
+		public onUpdate(method: (number) => void): number {
+			return this.updateCalls.push(method);
+		}
+
+		/**
+		 * Remove update call by array index
+		 * @param Number index
+		 * @return Boolean
+		 */
+		public removeUpdateCall(index: number): boolean {
+			index--;
+			
+			if (!this.updateCalls[index]) return false;
+
+			this.updateCalls[index] = null;
+
+			return true;
 		}
 	}
 }
--- src/game/main.ts
+++ src/game/main.ts
@@ -6,6 +6,9 @@ module Game {
 	export var layers: any = {};
 	export var loop: Core.Loop;
 
+	/**
+	 * Initialize the Tetris game
+	 */
 	export function start() {
 		Core.Log.info('Initialized game', 'Game/Main');
 		layers.background = new Core.Renderer('background', 800, 480, 1);
@@ -13,6 +16,24 @@ module Game {
 
 		loop = new Core.Loop();
 		loop.start();
+
+		var deg: number = 0;
+		var x, y;
+
+		loop.onUpdate(rate => {
+			deg += rate * 0.05;
+
+			Game.layers.background.draw(context => {
+				context.fillStyle = 'red';
+
+				x = Math.round(Math.cos(deg) * 150) + 200;
+				y = Math.round(Math.sin(deg) * 150) + 200;
+
+				context.fillRect(x, y, 30, 30);
+			});
+
+			Game.layers.background.render();
+		});
 	}
 
 	window.onload = Game.start;
