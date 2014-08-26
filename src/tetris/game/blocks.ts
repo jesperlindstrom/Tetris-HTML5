85c3bd4e599d6d055f94e156568d65e360c1e55a
Jesper Lindström <raxezdev@gmail.com>
2014-08-26 12:43:39 +0200

    Added block formations

--- src/tetris/game/blocks.ts
+++ src/tetris/game/blocks.ts
@@ -1,6 +1,42 @@
 module Game {
 	export class Blocks {
 		private static blocks: any = {};
+		private static blockSize = 40;
+		private static blockFormations: any = {
+			light_blue: [
+				[0,0], [1,0], [2,0], [3,0]
+			],
+
+			blue: [
+				[0,0],
+				[0,1], [1,1], [2,1], [3,1]
+			],
+
+			orange: [
+				                     [3,0],
+				[0,1], [1,1], [2,1], [3,1]
+			],
+
+			yellow: [
+				[0,0], [1,0],
+				[0,1], [1,1]
+			],
+
+			green: [
+				       [1,0], [2,0],
+				[0,1], [1,1]
+			],
+
+			purple: [
+				       [1,0],
+				[0,1], [1,1], [2,1]
+			],
+
+			red: [
+				[0,0], [1,0],
+				       [1,1], [2,1]
+			],
+		};
 
 		public static preload() {
 			Core.Assets.queue('block_light_blue', 'assets/block_light_blue.png');
@@ -23,7 +59,9 @@ module Game {
 
 			Game.loop.onUpdate((rate) => {
 				Game.layers.game.draw((context) => {
-					context.drawImage(this.blocks.green, 50, 50, 40, 40);
+					this.blockFormations.green.forEach(block => {
+						context.drawImage(this.blocks.green, block[0] * this.blockSize, block[1] * this.blockSize, this.blockSize, this.blockSize);
+					});
 				});
 			});
 		}
