8d545d565afa839abc4d5df51346db51dd4eacfb
Jesper Lindström <raxezdev@gmail.com>
2014-09-16 08:08:07 +0200

    Basic collision detection

--- src/tetris/core/collision.ts
+++ src/tetris/core/collision.ts
@@ -6,6 +6,10 @@ module Core {
 		 * @param Array coordinates
 		 */
 		public static test(grid, coordinates) {
+			if (grid[coordinates.x] && grid[coordinates.y]) {
+				return true;
+			}
+
 			return false;
 		}
 	}
